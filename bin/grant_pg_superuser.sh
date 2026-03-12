3b282b239cff25299a18423d434db4a2436db50c
josevilanir <vilanirneto@hotmail.com>
2026-03-12 11:10:48 -0300

    chore: remove unused deploy scripts, kamal configs and setup scripts

--- bin/grant_pg_superuser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Grant SUPERUSER to the 'rails' PostgreSQL role so Rails can disable FK
-# constraints during fixture loading in tests.
-sudo -u postgres psql -c "ALTER ROLE rails SUPERUSER;"
-echo "Done! Now run: bin/rails test"
